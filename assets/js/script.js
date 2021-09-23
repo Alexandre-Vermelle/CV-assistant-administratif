fdb2a7352447d941564f140c5b4a8c8550d12df9
Alexandre VERMELLE <alexandre.vermelle@gmail.com>
2021-09-23 18:19:36 +0200

    maj

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,7 +113,7 @@ function sanefPasswordSubmit() {
     let sanefPasswordtest = 'autoroutes'
 
     if (sanefPassword.value === 'autoroutes') {
-        alert("Mes missions au sein du Groupe SANEF étaient les suivantes : En charge de la gestion administrative du Pôle Information Trafic, j'étais l'unique point d'entrée des mails extérieurs (ministères, préfectures, DIR, CRICR) et des appels (clients). Je retransmettais toutes ces informations aux acteurs concernés (interne/externe) et j'organisais des réunions notamment lors des situations de crise. De ce fait, j'étais l'interlocuteur privilégié de la direction du Groupe, des autorités zonales et centrales, des clients, des animateurs de la radio 107.7 FM, de certains médias (journaux locaux, France 2...). Je contrôlais la cohérence des informations trafic et je les consolidais par le biais d'alertes trafic, de synthèses trafic, de messages sur les réseaux sociaux (Twitter, WAZE, application Sanef mobile). Par ailleurs, j'analysais les données trafic et je réalisais des rapports remis à la Direction des Infrastructures de Transport. Suite à une réorganisation, je fus par la suite en charge de l'exploitation des tunnels de Roissy (95), Saint Germain en Laye (78), Hardelot (62) et Gometz (91). Je devais effectuer les fermetures d'urgence de ces ouvrages en cas de détection de pannes techniques, d'incendie, de véhicules roulant à contresens entre autres. Par conséquent, j'assurais la sécurité des agents sur le terrain et des clients. Ce travail était posté en 3x8.");
+        alert("* Je gérais administrativement le Pôle Information Trafic du Groupe. \n* Je rédigeais des alertes trafic, des synthèses, des communiqués dans les délais impartis. \n* Je diffusais des informations trafic sur les réseaux sociaux (Twitter, WAZE, notifications sur les mobiles des abonnés) \n* J'étais en charge de la messagerie et étais l'unique point d'entrée des mails extérieurs (ministères, préfectures, DIR, CRICR) \n* Je retransmettais les informations importantes aux acteurs concernés (interne/externe) \n* J'étais l'interlocuteur privilégié de la Direction du Groupe, des autorités, des animateurs de la radio 107.7 FM. \n* J'organisais des réunions notamment lors des situations de crise. \n* Je mettais à jour les plannings d'astreinte des directeurs et cadres d'exploitation \n* J’analysais des données trafic et réalisais des rapports pour la DIT. \n* Je répondais aux appels téléphoniques (préfectures, gendarmeries, pompiers, PC Sécurité, Direction) et étais l’unique point d’entrée des appels clients. \n* Je parlais anglais aux usagers étrangers en détresse sur une autoroute. \n* Je supervisais toute l'information trafic du Groupe (panneaux à message variable, radio 107.7 FM) \n* J'exploitais plusieurs tunnels (Roissy, Hardelot, Saint Germain en Laye, Gometz) et les fermais en cas d'incident ou de panne technique (contresens, accident, incendie…)");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -125,7 +125,7 @@ function sncfPasswordSubmit() {
     let sncfPasswordtest = 'trains'
 
     if (sncfPassword.value === 'trains') {
-        alert("Mes missions pour la SNCF étaient les suivantes : J'assurais la gestion du trafic ferroviaire dans un poste d'aiguillage notamment dans les gares de Lens (62), Béthune (62) et Pont à Vendin (62). Je déclenchais les mesures d'urgence en cas d'incident grave. Je garantissais la maintenance et l'entretien des systèmes d'aiguillage. J'y ai également débuté une formation d'agent de circulation. Ce travail était posté en 3x8.");
+        alert("* J'assurais la gestion du trafic ferroviaire en poste d'aiguillage dans les gares de Lens (62), Béthune (62) et Pont à Vendin (62). \n* Je déclenchais les mesures d'urgence en cas d'incident grave. \n* Je garantissais la maintenance et l'entretien des systèmes d'aiguillage. \n* J'y ai également débuté une formation d'agent de circulation. \n* Ce travail était posté en 3x8.");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -137,7 +137,7 @@ function formationPasswordSubmit() {
     let formationPasswordtest = 'formation'
 
     if (formationPassword.value === 'formation') {
-        alert("Mes missions au CFPPA de Sains du Nord furent les suivantes : J'assurais la gestion administrative et pédagogique d'une formation intitulée CS Tourisme. J'encadrais l'équipe éducative et je réalisais les planning. J'étais formateur auprès d'un groupe d'adultes et leur enseignais la méthodologie de projet et la sociologie du monde rural. Je les ai suivi dans la mise en place de leur projet professionnel et leur ai rendu visite lors de leur stage. J'ai organisé les sessions d'examens et j'ai préparé le recrutement des apprenants de la session suivante. J'ai commencé à étudier la faisabilité de la création d'un centre de formation transfrontalier en coopération avec la société 'Inialité' basée à Hirson (02) dans le cadre du développement du territoire en matière de formation notamment dans le domaine du tourisme.");
+        alert("* J'assurais la gestion administrative et pédagogique d'une formation intitulée CS Tourisme. \n* J'encadrais l'équipe éducative et je réalisais les plannings. \n* J'étais formateur auprès d'un groupe d'adultes et leur enseignais la méthodologie de projet et la sociologie du monde rural. \n* J'ai suivi les apprenants dans la mise en place de leur projet professionnel et leur ai rendu visite lors de leur stage. \n* J'ai organisé les sessions d'examens. \n* J'ai préparé le recrutement des apprenants de la session suivante (courriers, entretiens individuels) \n* J'ai étudié la faisabilité de la création d'un centre de formation transfrontalier en coopération avec la société 'Inialité' basée à Hirson (02) dans le cadre du développement du territoire.");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -149,7 +149,7 @@ function educationPasswordSubmit() {
     let educationPasswordtest = 'éducation'
 
     if (educationPassword.value === 'éducation') {
-        alert("Mes missions étaient les suivantes : J'ai été assistant d'éducation au Lycée Saint Dominique de Béthune (62) et dans les Collèges Saint Vaast de Béthune (62) et Bernard Chochoy de Norrent Fontes (62). J'étais en charge de la surveillance des élèves durant les permanences, les récréations, les repas. J'assurais également leur sécurité à l'extérieur de ces établissements scolaires (passage piétons, acccompagnement lors des cross...)");
+        alert("* J'ai été assistant d'éducation au Lycée Saint Dominique de Béthune (62) et dans les Collèges Saint Vaast de Béthune (62) et Bernard Chochoy de Norrent Fontes (62). \n* J'étais en charge de la surveillance des élèves durant les permanences, les récréations, les repas. \n* J'assurais également leur sécurité à l'extérieur de ces établissements scolaires (passage piétons, acccompagnement lors des cross...)");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -161,7 +161,7 @@ function teacherPasswordSubmit() {
     let teacherPasswordtest = 'professeur'
 
     if (teacherPassword.value === 'professeur') {
-        alert("Mes missions au Collège Villars étaient les suivantes : Je travaillais auprès des sections SEGPA. J'enseignais l'horticulture aux 4èmes/3èmes et l'informatique aux 6èmes/5èmes en favorisant les travaux pratiques");
+        alert("* Je travaillais auprès des sections SEGPA. \n* J'enseignais l'horticulture aux 4èmes/3èmes en favorisant les travaux pratiques. \n * J'inculquais l'informatique aux 6èmes/5èmes (Initiation sur Word)");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -173,7 +173,7 @@ function factoryPasswordSubmit() {
     let factoryPasswordtest = 'aciérie'
 
     if (factoryPassword.value === 'aciérie') {
-        alert("Mes missions pour Arcelor Mittal (anciennement Sollac) étaient les suivantes : Je fus employé en tant que vacancier durant les étés 1999, 2000, 2003 et 2004. J'étais chargé de remettre en état de production les poches servant à la fusion de l'acier dans le secteur des hauts fourneaux de l'usine. Ce travail était posté en 3x8.");
+        alert("* Je fus employé en tant que vacancier durant les étés 1999, 2000, 2003 et 2004. \n* J'étais chargé de remettre en état de production les poches servant à la fusion de l'acier dans le secteur des hauts fourneaux de l'usine. \n* Ce travail était posté en 3x8.");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -184,7 +184,7 @@ function officePasswordSubmit() {
     let officePasswordtest = 'tourisme'
 
     if (officePassword.value === 'tourisme') {
-        alert("Mes missions à l'Office de tourisme d'Aire sur la Lys étaient les suivantes : J'assurais la tenue de l'Office de Tourisme (Gestion administrative, Accueil des touristes,...). J'animais des événements lors de salons ou de fêtes (Salon Tourissima à Lille, Brocante, Marché de Noël...). J'ai mis en place des partenariats avec les acteurs locaux et j'ai créé des supports de communication (dépliants, plaquettes...). J'ai rédigé un mémoire sur la problématique 'Comment attirer plus de touristes sur la ville d'Aire sur la Lys ?'. A la suite de mon stage, je suis resté membre bénévole jusqu'en 2009.");
+        alert("* J’accueillais les touristes et répondais à leur demande. \n* Je m’exprimais en anglais quand des touristes étrangers se présentaient. \n* Je répondais aux mails et gérais la messagerie. \n* Je répondais aux appels téléphoniques. \n* Je réceptionnais les courriers et assurais le suivi (réponses, classement, archivage) \n* Je vendais des produits touristiques ou des places de spectacles. \n* J'organisais des événements locaux (Brocante, Marché de Noël...) \n* J'animais des événements lors de salons ou de fêtes (Salon Tourissima à Lille, Brocante, Marché de Noël...) \n* J'ai mis en place des partenariats avec les acteurs locaux. \n* J'ai créé des supports de communication (dépliants, plaquettes...) \n* J'ai rédigé un mémoire sur la problématique 'Comment attirer plus de touristes sur la ville d'Aire sur la Lys ?’.");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -195,7 +195,7 @@ function cityPasswordSubmit() {
     let cityPasswordtest = 'territoire'
 
     if (cityPassword.value === 'territoire') {
-        alert("Lors de ce stage à la Communauté de Communes Artois Flandres, j'avais pour mission de créer et de réhabiliter des sentiers de randonnée. Pour cela, j'ai réalisé un diagnostic de territoire et fait des demandes de subventions.");
+        alert("* J’ai réalisé une étude sur la création et la réhabilitation de sentiers de randonnée. \n* J’ai envoyé des courriers et fait des demandes de subventions. \n* Je passais des appels téléphoniques. \n* Je scannais, photocopiais ou faxais des documents. \n* J’ai réalisé un diagnostic de territoire. \n* J’ai interrogé la population via des questionnaires. \n* J’ai fait des repérages sur le terrain.");
     }
     else {
         alert('Le mot de passe est incorrect');
@@ -206,7 +206,7 @@ function handicapPasswordSubmit() {
     let cityPasswordtest = 'handicap'
 
     if (cityPassword.value === 'handicap') {
-        alert("Durant mon stage au CAT d'Isbergues, j'encadrais un groupe d'adultes handicapés dans différentes activités (maçonnerie, entretien des espaces verts, menuiserie, conditionnement). J'animais des activités sportives ou culturelles (VTT, football, visite de Clairmarais...).");
+        alert("* J'encadrais un groupe d'adultes handicapés dans différentes activités (maçonnerie, espaces verts, menuiserie, conditionnement). \n* J'animais des activités sportives ou culturelles (VTT, football, visite de Clairmarais...).");
     }
     else {
         alert('Le mot de passe est incorrect');
